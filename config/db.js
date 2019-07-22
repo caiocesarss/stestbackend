46806b059929b2fd391aab6ad74d379442ad0941
Caio Siqueira <caiosiqueira@outlook.com>
2019-07-22 00:40:31 -0300

    adjust db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-        host: '0.0.0.0',
-        user: 'root',
-        password: 'mysql123',
-        database: 'samaiait'
+        host: 'db',
+        user: 'mysql',
+        password: 'supersecret',
+        database: 'samtest'
     }
 });
 
